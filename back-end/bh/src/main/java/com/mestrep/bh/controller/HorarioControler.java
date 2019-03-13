8695eec964b27076ce976c4bb90062779bd2ffd0
Patrick Silva <patrick.silva@multify.app>
2019-03-13 11:03:30 -0300

    Adicionando horario por usuario logado e listando por id do usuario

--- back-end/bh/src/main/java/com/mestrep/bh/controller/HorarioControler.java
+++ back-end/bh/src/main/java/com/mestrep/bh/controller/HorarioControler.java
@@ -3,8 +3,10 @@ package com.mestrep.bh.controller;
 import com.mestrep.bh.model.Horario;
 import com.mestrep.bh.services.HorarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -18,13 +20,19 @@ public class HorarioControler {
         this.horarioService = horarioService;
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USUARIO')")
     @PostMapping(value = "/horario")
-    public Horario salvarHorario(@RequestBody Horario horario) {
-        return horarioService.salvar(horario);
+    public Horario salvarHorario(@RequestBody Horario horario, Principal principal) {
+        return horarioService.salvar(horario, principal.getName());
     }
 
     @GetMapping(value = "/horarios")
     public List<Horario> listarTodosHorarios(){
         return horarioService.listarTodos();
+    }
+
+    @GetMapping(value = "/horarios/{id}")
+    public List<Horario> listarTodosHorarios(@PathVariable("id") Integer id){
+        return horarioService.listarTodosHorariosDeUsuarioPorId(id);
     }
 }
