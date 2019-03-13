d0746118223f4daa0ac284840d402b1b87da4768
Patrick Silva <patrick.silva@multify.app>
2019-03-13 11:13:08 -0300

    Modificando valores do swagger

--- back-end/bh/src/main/java/com/mestrep/bh/controller/UsuarioController.java
+++ back-end/bh/src/main/java/com/mestrep/bh/controller/UsuarioController.java
@@ -2,7 +2,11 @@ package com.mestrep.bh.controller;
 
 import com.mestrep.bh.model.Usuario;
 import com.mestrep.bh.services.UsuarioService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,12 +22,15 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
-    @PostMapping(value = "/usuario")
+    @ApiOperation(value = "Inseri um usuário. Só quem pode inserir um usuário é um administrador")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PostMapping(value = "/usuario", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Usuario salvarUsuario(@RequestBody Usuario usuario){
         return usuarioService.salvar(usuario);
     }
 
-    @GetMapping(value = "/usuarios")
+    @ApiOperation(value = "Lista de usuário")
+    @GetMapping(value = "/usuarios", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Usuario> listarUsuarios(){
         return usuarioService.listarTodos();
     }
