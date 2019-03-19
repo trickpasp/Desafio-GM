7e35272f3523e1e4868bf0d0383b6220c1a995df
Patrick Silva <patrick.silva@multify.app>
2019-03-19 06:24:02 -0300

    Obtendo a usuário da sessão

--- back-end/bh/src/main/java/com/mestrep/bh/security/UserServiceSec.java
+++ back-end/bh/src/main/java/com/mestrep/bh/security/UserServiceSec.java
@@ -5,7 +5,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class UserServiceSec {
 
-    public static Usuario autheticated(){
+    //Metodo retorna o usuario da sessao
+    public static Usuario authenticated(){
         try{
             return (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         }catch (Exception e){
