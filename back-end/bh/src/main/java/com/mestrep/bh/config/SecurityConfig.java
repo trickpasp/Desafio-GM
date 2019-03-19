1bbb3e3d8182aab30dbbf04e371a0176f137c4cd
Patrick Silva <patrick.silva@multify.app>
2019-03-19 06:26:47 -0300

    Adicionando permissão de swagger e h2 e permissões de cors

--- back-end/bh/src/main/java/com/mestrep/bh/config/SecurityConfig.java
+++ back-end/bh/src/main/java/com/mestrep/bh/config/SecurityConfig.java
@@ -1,7 +1,8 @@
 package com.mestrep.bh.config;
 
-import com.mestrep.bh.util.JWTAuthenticationFilter;
-import com.mestrep.bh.util.JWTAuthorizationFilter;
+import com.mestrep.bh.security.JWTAuthenticationFilter;
+import com.mestrep.bh.security.JWTAuthorizationFilter;
+import com.mestrep.bh.services.UserDetailsServiceImp;
 import com.mestrep.bh.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +22,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -28,8 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //Metodos gets privado
     private static final String[] PUBLIC_MATCHERS_GET = {
             "/app/usuarios",
-            "/app/horarios/**",
-            "/**"
+            "/app/horarios/**"
     };
     //Metodos post publicos
     private static final String[] PUBLIC_MATCHERS_POST = {
@@ -44,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JWTUtil jwtUtil;
 
     @Autowired
-    public SecurityConfig(@Lazy UserDetailsService userDetailsService, JWTUtil jwtUtil) {
+    public SecurityConfig(@Lazy UserDetailsServiceImp userDetailsService, JWTUtil jwtUtil) {
         this.userDetailsService = userDetailsService;
         this.jwtUtil = jwtUtil;
     }
@@ -58,10 +61,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                .antMatchers("/h2-console/**/**").permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
         http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // Permitindo swagger para acessar sem permissao
+        web.ignoring().antMatchers("/v2/api-docs")//
+                .antMatchers("/swagger-resources/**")//
+                .antMatchers("/swagger-ui.html")//
+                .antMatchers("/configuration/**")//
+                .antMatchers("/webjars/**")//
+                .antMatchers("/public")
+
+                // Permitindo h2 para acessar sem permissao
+                .and()
+                .ignoring()
+                .antMatchers("/h2-console/**/**");;
     }
 
     //Método usado para autenticação de usuario
@@ -73,8 +93,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //Cors, utilizado para permitir cross-origin, é util para receber requisicao de qualquer origin, que por padrão, o spring desativa
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
