a94c81d52ce2ca4fe3b80ef5088b0f34863a7179
Patrick Silva <patrick.silva@multify.app>
2019-03-19 06:28:41 -0300

    Adicionando pesquisar por id de usuario

--- back-end/bh/src/main/java/com/mestrep/bh/repository/HorarioRepository.java
+++ back-end/bh/src/main/java/com/mestrep/bh/repository/HorarioRepository.java
@@ -4,6 +4,10 @@ import com.mestrep.bh.model.Horario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HorarioRepository extends JpaRepository<Horario, Integer> {
+
+    List<Horario> findAllByUsuario_Id(Integer id);
 }
